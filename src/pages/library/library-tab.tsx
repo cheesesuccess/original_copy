73a4a67c670727e9e633bf05c5b1cc5701a7ea1b
cheesesuccess <natashasibanda2025@outlook.com>
2025-08-30 13:05:59 +0200

    Update library-tab.tsx

--- src/pages/library/library-tab.tsx
+++ src/pages/library/library-tab.tsx
@@ -1,10 +1,13 @@
 import { createMemo, VoidComponent } from 'solid-js'
 import { Dynamic } from 'solid-js/web'
-import { sortByKey } from '../../utils'
-import { useEntitiesStore, useLibraryStore } from '../../stores/stores'
-import { BaseMusicItem, MusicItemType, Track } from '../../types/types'
-import { LibraryPageConfig } from './config'
 import { ScrollContainer } from '../../components/scroll-container/scroll-container'
+import {
+  useEntitiesStore,
+  useLibraryStore
+} from '../../stores/stores'
+import { BaseMusicItem, MusicItemType, Track } from '../../types/types'
+import { sortByKey } from '../../utils'
+import { LibraryPageConfig } from './config'
 import * as styles from './library.css'
 
 export const LibraryPage: VoidComponent<LibraryPageConfig> = (props) => {
@@ -13,14 +16,14 @@ export const LibraryPage: VoidComponent<LibraryPageConfig> = (props) => {
 
   const itemsSelector = () => {
     switch (props.type) {
+      case MusicItemType.TRACK:
+        return dataState.tracks
       case MusicItemType.ALBUM:
         return dataState.albums
       case MusicItemType.ARTIST:
         return dataState.artists
       case MusicItemType.PLAYLIST:
         return dataState.playlists
-      case MusicItemType.HISTORY:
-        return {} // History component generates its own items from player store
       default:
         throw new Error('Wrong item type')
     }
@@ -28,8 +31,8 @@ export const LibraryPage: VoidComponent<LibraryPageConfig> = (props) => {
 
   const itemIds = createMemo(() => {
     const itemsArray = sortByKey(
-      [...Object.values<BaseMusicItem>(itemsSelector() as { [key: string]: BaseMusicItem })],
-      libraryState.sortKeys[props.type] as keyof BaseMusicItem,
+      [...Object.values<Track>(itemsSelector() as { [key: string]: Track })],
+      libraryState.sortKeys[props.type] as keyof Track,
     ) as unknown as BaseMusicItem[]
 
     return itemsArray.map((item) => item.id)
