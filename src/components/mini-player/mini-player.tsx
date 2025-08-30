9495eee22f588732320d5bf4deabc1eaea48069e
cheesesuccess <natashasibanda2025@outlook.com>
2025-08-30 09:00:40 +0200

    Update mini-player.tsx

--- src/components/mini-player/mini-player.tsx
+++ src/components/mini-player/mini-player.tsx
@@ -3,12 +3,13 @@ import { Show, VoidComponent } from 'solid-js'
 import { createMediaQuery } from '~/helpers/hooks/create-media-query'
 import { Artwork } from '../shared-player-components/artwork/artwork'
 import { Controls } from '../shared-player-components/controls/main-controls'
+import { PlayNextButton } from '../shared-player-components/controls/play-next-prev-buttons'
 import { PlayPauseButton } from '../shared-player-components/controls/play-pause-button'
-import { SeekBackButton } from '../shared-player-components/controls/seek-back-button'
 import { VolumePanel } from '../shared-player-components/volume/volume-panel'
 import { Info } from '../shared-player-components/info/info'
 import { Timeline } from '../shared-player-components/timeline/timeline'
 import { clx } from '~/utils'
+import { FavoriteButton } from '../shared-player-components/info/favorite-button'
 import { Icon } from '../icon/icon'
 import * as styles from './mini-player.css'
 
@@ -18,6 +19,9 @@ export interface MiniPlayerProps {
 
 export const MiniPlayer: VoidComponent<MiniPlayerProps> = (props) => {
   const areMainControlsHidden = createMediaQuery(styles.COMPACT_MEDIA)
+
+  const areSecondaryActionsVisible = createMediaQuery('(min-width: 421px)')
+  const isPlayPauseButtonVisible = createMediaQuery('(min-width: 300px)')
 
   const navigate = useNavigate()
 
@@ -37,17 +41,19 @@ export const MiniPlayer: VoidComponent<MiniPlayerProps> = (props) => {
           </div>
           <Info />
         </button>
+
+        {areSecondaryActionsVisible() && <FavoriteButton />}
       </div>
       <Show
         when={!areMainControlsHidden()}
         fallback={
-          <div class={styles.compactControls}>
-            <SeekBackButton simple />
-            <PlayPauseButton simple />
-          </div>
+          <>
+            {isPlayPauseButtonVisible() && <PlayPauseButton />}
+            {areSecondaryActionsVisible() && <PlayNextButton />}
+          </>
         }
       >
-        <Controls simple class={styles.miniPlayerControls} />
+        <Controls />
         <VolumePanel />
       </Show>
     </div>
