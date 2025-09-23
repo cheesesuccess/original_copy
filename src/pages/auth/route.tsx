25cebc9cbef2986f9eb8fb0e1ffe5f1d3c8fb1bd
cheesesuccess <natashasibanda2025@outlook.com>
2025-09-23 10:04:37 +0200

    Update route.tsx

--- src/pages/auth/route.tsx
+++ src/pages/auth/route.tsx
@@ -1,5 +1,5 @@
 import { RouteDefinition } from 'solid-app-router'
-import AuthPage from './auth'
+import AuthPage from './auth-v1'
 
 const authRoute: RouteDefinition = {
   path: '/auth-v1',                           
