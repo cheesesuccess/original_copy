d9fae5a105d1141f0ff2677d8b2b974358e026d6
cheesesuccess <natashasibanda2025@outlook.com>
2025-09-23 10:55:30 +0200

    Update firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,6 @@
 // src/firebase/firebase.ts
 import { initializeApp, getApps } from 'firebase/app'
-import { getAuth } from 'firebase/auth-v1'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,5 +14,5 @@ const firebaseConfig = {
 }
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig)
-export const auth-v1 = getAuth(app)
+export const auth = getAuth(app)
 export default app
